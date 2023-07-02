3c63e1be16b935334cdf431e2d33ef94dd8e4389
samualmoot <samual.r.moot@gmail.com>
2023-07-02 12:46:53 -0500

    dependency management

--- web/core/views.py
+++ web/core/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from rest_framework import viewsets
+from web.core import models
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the card index.")
+
+
+class DeckViewSet(viewsets.ModelViewSet):
+    queryset = models.Deck.objects.all()
