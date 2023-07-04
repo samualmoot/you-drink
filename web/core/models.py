e72e74fc4c525b83646a35d5e7a948e7de606a42
samualmoot <samual.r.moot@gmail.com>
2023-07-04 08:16:40 -0500

    user tables

--- web/core/models.py
+++ web/core/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Deck(models.Model):
@@ -11,3 +14,42 @@ class Card(models.Model):
     drink_amount = models.IntegerField(default=1)
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE, default=None)
     type = models.CharField(max_length=30, default=None)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    deck_library = models.ManyToManyField(Deck)
+    card_library = models.ManyToManyField(Card)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+class Friend(models.Model):
+    users = models.ManyToManyField(User)
+    current_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner", null=True)
+
+    @classmethod
+    def make_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(
+            current_user = current_user
+        )
+        friend.users.add(new_friend)
+
+    @classmethod
+    def remove_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(
+            current_user = current_user
+        )
+        friend.users.remove(new_friend)
+
+    def __str__(self):
+        return str(self.current_user)
---
+++ web/core/types.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class RelationshipType(Enum):
+    FAMILY = 'family'
+    FRIEND = 'friend'
+    
